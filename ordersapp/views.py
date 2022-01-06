ca67f4ca02362a66b2c80f8d9d54911e932be46b
Roman <mamchiy@yandex.ru>
2022-01-06 13:49:11 +0300

    creating application controllers ordersapp

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView, TemplateView
+
+
+class OrderListView(ListView):
+    pass
+
+
+class OrderDetailView(DetailView):
+    pass
+
+
+class OrderUpdateView(UpdateView):
+    pass
+
+
+class OrderCreateView(CreateView):
+    pass
+
+
+class OrderDeleteView(DeleteView):
+    pass
+
+
+def order_forming_complete(request, pk):
+    pass
