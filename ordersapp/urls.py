f42b61ae989226ef05791293010468614abb18af
Roman <mamchiy@yandex.ru>
2022-01-25 21:21:21 +0300

    pulling up prices in cart orders

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from ordersapp.views import OrderListView, OrderCreateView, OrderDeleteView, OrderDetailView, \
-    OrderUpdateView, order_forming_complete
+    OrderUpdateView, order_forming_complete, get_product_price
 
 app_name = 'ordersapp'
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('read/<int:pk>/', OrderDetailView.as_view(), name='read'),
     path('delete/<int:pk>/', OrderDeleteView.as_view(), name='delete'),
     path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
+    path('product/<int:pk>/price/', get_product_price, name='get_product_price'),
 ]
